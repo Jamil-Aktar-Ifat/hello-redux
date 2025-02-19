d1a6bc24a96ac3c59c4dbbbfa89bc9e2c92e3d33
jamil-dev <i>
2025-02-19 19:47:31 +0000

    actions written

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+
 function App() {
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
     <>
       <div>
         <div className="text-center mt-20 space-y-4">
           <h2 className="text-2xl underline">Counter with Redux</h2>
-          <button className="btn">Increment</button>
+          <button className="btn" onClick={handleIncrement}>
+            Increment
+          </button>
           <div>0</div>
-          <button className="btn">Decrement</button>
+          <button className="btn" onClick={handleDecrement}>
+            Decrement
+          </button>
         </div>
       </div>
     </>
