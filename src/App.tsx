d9f7f320e53ccbab526311db0fdf128520b9a575
jamil-dev <i>
2025-02-19 19:09:32 +0000

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <div>
+        <div className="text-center mt-20 space-y-4">
+          <h2 className="text-2xl underline">Counter with Redux</h2>
+          <button className="btn">Increment</button>
+          <div>0</div>
+          <button className="btn">Decrement</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
