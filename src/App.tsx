f7b09a7f6254bccc1a2288f6f05ec6835f15e046
jamil-dev <i>
2025-02-21 20:44:02 +0000

    route set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Navbar from "./components/layout/Navbar";
 import { Button } from "./components/ui/button";
 import { decrement, increment } from "./redux/features/counter/counterSlice";
 import { useAppDispatch, useAppSelector } from "./redux/hook";
@@ -17,26 +18,19 @@ function App() {
   return (
     <>
       <div>
+        <Navbar></Navbar>
         <div className="text-center mt-20 space-y-4">
           <h2 className="text-2xl ">
             Counter with Redux: <span className="text-blue-400"> {count}</span>
           </h2>
           <div className="flex gap-3 justify-center">
-            <Button onClick={() => handleIncrement(1)}>
-              Increment
-            </Button>
-            <Button onClick={() => handleIncrement(5)}>
-              Increment by 5
-            </Button>
+            <Button onClick={() => handleIncrement(1)}>Increment</Button>
+            <Button onClick={() => handleIncrement(5)}>Increment by 5</Button>
           </div>
 
           <div className="flex justify-center gap-3">
-            <Button onClick={() => handleDecrement(3)}>
-              Decrement by 3
-            </Button>
-            <Button onClick={() => handleDecrement(1)}>
-              Decrement
-            </Button>
+            <Button onClick={() => handleDecrement(3)}>Decrement by 3</Button>
+            <Button onClick={() => handleDecrement(1)}>Decrement</Button>
           </div>
         </div>
       </div>
