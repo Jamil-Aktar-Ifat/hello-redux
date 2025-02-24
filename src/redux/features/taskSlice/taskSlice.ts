78291cbe404b536bd6039586a955c660cf5a7324
jamil-dev <i>
2025-02-24 19:13:51 +0000

    card view created for tasks

--- src/redux/features/taskSlice/taskSlice.ts
+++ src/redux/features/taskSlice/taskSlice.ts
@@ -4,7 +4,7 @@ import { RootState } from "../../store";
 
 interface InitialState {
   tasks: ITask[];
-  filter: "all" | "high" | "medium" | "low";
+  //   filter: "all" | "high" | "medium" | "low";
 }
 
 const initialState: InitialState = {
@@ -19,14 +19,14 @@ const initialState: InitialState = {
     },
     {
       id: "asdfgjhjs",
-      title: "Initialize Frontend",
-      description: "Create Homepage, and routing",
+      title: "Initialize Github-repo",
+      description: "Create Stage branch",
       dueDate: "2025-02-25",
       isCompleted: false,
-      priority: "High",
+      priority: "Low",
     },
   ],
-  filter: "all",
+  //   filter: "all",
 };
 
 const taskSlice = createSlice({
@@ -38,8 +38,8 @@ const taskSlice = createSlice({
 export const selectTasks = (state: RootState) => {
   return state.todo.tasks;
 };
-export const selectFilter = (state: RootState) => {
-  return state.todo.filter;
-};
+// export const selectFilter = (state: RootState) => {
+//   return state.todo.filter;
+// };
 
 export default taskSlice.reducer;
